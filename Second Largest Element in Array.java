555ab58b2e6a24458acf2c0afb51bb857540c62d
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-21 12:19:30 +0530

    Update Second Largest Element in Array.java

--- Second Largest Element in Array.java
+++ Second Largest Element in Array.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+public class SecondLargestElementInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (array[i] > largest) {
+                secondLargest = largest;
+                largest = array[i];
+            } else if (array[i] > secondLargest && array[i] != largest) {
+                secondLargest = array[i];
+            }
+        }
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element.");
+        } else {
+            System.out.println("The second largest element in the array is: " + secondLargest);
+        }
+        scanner.close();
+    }
+}
 
